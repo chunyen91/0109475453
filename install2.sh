ccfa5af3189d1e4435dffee2d47dfb351bf4034d
chunyen91 <32806435+chunyen91@users.noreply.github.com>
2017-12-10 04:03:35 +0800

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
 myint=`ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}'`;
-curl -s -o ip.txt https://raw.githubusercontent.com/aabell3/newdeb/master/ip.txt
-find=`grep $myip ip.txt`
-if [ "$find" = "" ]
-then
 clear
 
 #info
